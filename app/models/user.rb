6bd4c148a68917754c68c2992106b90ac78a0a36
wale-prog <olapetanwale@outlook.com>
2022-09-20 15:50:31 +0100

    Fix changes required by code reviewer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  after_initialize :init
+
   # Validations
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -11,5 +13,10 @@ class User < ApplicationRecord
   # Actions
   def recent_three_posts
     posts.order('created_at').last(3)
+  end
+
+  def init
+    self.posts_counter ||= 0
+    true
   end
 end
