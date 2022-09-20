ded66f2ac4d8edf3087238d384c4a7ff1597c1ce
wale-prog <olapetanwale@outlook.com>
2022-09-20 15:52:54 +0100

    Fix linter errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Like, type: :model do
     @post = Post.create(title: 'Rails rock', text: 'Rails presents the best developer\'s tools', author_id: @author.id)
     @like = Like.create(post_id: @post.id, author_id: @author.id)
   end
-  
-    it 'like object should be valid' do
-      expect(@like).to be_valid
-    end
 
-    context 'Custom method' do
-      it 'Should update the likes' do
-        expect(@like.update_like_counter).to eq(@post.update(likes_counter: @post.likes.count))
-      end
+  it 'like object should be valid' do
+    expect(@like).to be_valid
+  end
+
+  context 'Custom method' do
+    it 'Should update the likes' do
+      expect(@like.update_like_counter).to eq(@post.update(likes_counter: @post.likes.count))
     end
+  end
 end
