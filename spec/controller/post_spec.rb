9decec6903c05538c560f8525695edbd26b4cd2e
wale-prog <olapetanwale@outlook.com>
2022-09-20 10:01:14 +0100

    Fix linter errors

--- spec/controller/post_spec.rb
+++ spec/controller/post_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-	describe 'GET #index' do
-		before(:example) do
-			get '/users/1/posts'
-		end
-		it 'is a success' do
-			expect(response).to have_http_status(:ok)
-		end
+  describe 'GET #index' do
+    before(:example) do
+      get '/users/1/posts'
+    end
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
 
-		it 'should include the template text' do
-			expect(response.body).to include('Posts listing for all posts in the database')
-		end
+    it 'should include the template text' do
+      expect(response.body).to include('Posts listing for all posts in the database')
+    end
 
-		it 'should render template correctly' do
-			expect(response).to render_template(:index)
-		end
-	end
+    it 'should render template correctly' do
+      expect(response).to render_template(:index)
+    end
+  end
 
-	describe 'GET #show' do
-		before(:example) do
-			get '/users/1/posts/2'
-		end
+  describe 'GET #show' do
+    before(:example) do
+      get '/users/1/posts/2'
+    end
 
-		it 'should render template correctly' do
-			expect(response).to render_template(:show)
-		end
+    it 'should render template correctly' do
+      expect(response).to render_template(:show)
+    end
 
-		it 'should include the template text' do
-			expect(response.body).to include ('Post listing for an individually selected post')
-		end
+    it 'should include the template text' do
+      expect(response.body).to include('Post listing for an individually selected post')
+    end
 
-		it 'should be successful' do
-			expect(response).to have_http_status(:ok)
-		end
-	end
+    it 'should be successful' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
--- spec/controller/user_spec.rb
+++ spec/controller/user_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-	describe 'GET /index' do
-		before(:example) { get users_path }
+  describe 'GET /index' do
+    before(:example) { get users_path }
 
-		it 'is a success' do 
-			expect(response).to have_http_status(:ok)
-		end
-
-    it 'Should include the template text' do
-			expect(response.body).to include('User listing page for all users')
-		end
-		
-		it 'renders index template' do
-			expect(response).to render_template(:index)
-		end
-	end
-
-	describe 'GET /show' do
-		before(:example) do
-			get '/users/1'
-		end
-
-		it 'is a success' do 
-			expect(response).to have_http_status(:ok)
-		end
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
 
     it 'Should include the template text' do
-			expect(response.body).to include('User listing page for a specific user')
-		end
-		
-		it 'renders show template' do
-			expect(response).to render_template(:show)
-		end
-	end
+      expect(response.body).to include('User listing page for all users')
+    end
+
+    it 'renders index template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Should include the template text' do
+      expect(response.body).to include('User listing page for a specific user')
+    end
+
+    it 'renders show template' do
+      expect(response).to render_template(:show)
+    end
+  end
 end
