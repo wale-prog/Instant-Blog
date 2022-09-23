4cc1612717def9adb707b80559920787dba7ab10
wale-prog <olapetanwale@outlook.com>
2022-09-23 22:33:50 +0100

    Implement add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = current_user.comments.new(comment_params)
+    @comment = current_user.comments.new(comment_params)    
+    @comment.post_id = params[:post_id]
+    @comment.update_comments_counter
+    if @comment.save
+      flash[:notice] = 'Comment created succefully'
+      redirect_to user_post_comments_path
+    end
   end
 
   private
